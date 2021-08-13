a8ee8c8a807d621ff2dbf396999965d3dd04eb69
namph <namph@hblab.vn>
2021-08-13 20:27:21 +0700

    setup connect db and log

--- src/main/java/org/namph/blog/BlogApplication.java
+++ src/main/java/org/namph/blog/BlogApplication.java
@@ -1,10 +1,14 @@
+package org.namph.blog;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-
+    final static Logger logger = Logger.getLogger(BlogApplication.class);
     public static void main(String[] args) {
+        logger.info("test log");
         SpringApplication.run(BlogApplication.class, args);
     }
 }
